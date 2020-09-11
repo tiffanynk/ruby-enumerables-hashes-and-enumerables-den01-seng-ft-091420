7af0aacf972d891c7cc2cf7542d91f1a8a56ee7d
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-09-11 19:48:07 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,9 +1,15 @@
 # birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
+#   "Timmy" => 9,
+#   "Sarah" => 6,
 #   "Amanda" => 27
 # }
 
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
+#using each to iterate over each pair of kids name/age
+#yielding the key/value pair to the block of code between the do/end keywords
+#by assigning the variables kids_name and age in between the pipes, | |,
+#to be the placeholders for each key/value pair.
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
 end
